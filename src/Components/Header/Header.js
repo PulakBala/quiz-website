0834188ea50128b3e08f1845cdd0c8e5489328c2
PulakBala <pulakbala0524@gmail.com>
2022-11-12 05:41:47 +0600

    added header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div className='bg-orange-200 h-28 flex text-3xl justify-evenly items-center'>
             <div className='font-bold text-4xl mr-16'>
-                <h1>Programming Hero Quiz</h1>
+                <h1>Web Development Quiz</h1>
             </div>
             <Link to='/'>Home</Link>
             <Link to='/topics'>Topics</Link>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import './Home.css'
 const Home = () => {
     return (
-        <div>
-            <h1>This is home page</h1>
+        <div className=''>
+            <div className='img'>
+                <h2 className='text-lg py-8 ml-96'>During the Quiz-test , <br/> do not open other tabs in the  browser or windows with the site "lms" <br/> Stay on this site until quiz-test is complete. Otherwise, it will be terminates with the status FAILED.</h2>
+            </div>
+            <div>
+               <h2>This is home page</h2>
+            </div>
         </div>
     );
 };
