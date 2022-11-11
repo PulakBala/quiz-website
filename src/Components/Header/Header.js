bb613da1a0181298b95bcbf7cb643210c4762b23
PulakBala <pulakbala0524@gmail.com>
2022-11-12 03:31:02 +0600

    add navbar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,10 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
+        <div className='bg-orange-200 h-28 flex text-3xl justify-evenly items-center'>
+            <div className='font-bold text-4xl mr-16'>
+                <h1>Programming Hero Quiz</h1>
+            </div>
             <Link to='/'>Home</Link>
             <Link to='/topics'>Topics</Link>
             <Link to='/statistics'>Statistics</Link>
