3d294abd528f5ddd35a68ca45cb0146d40e57483
PulakBala <pulakbala0524@gmail.com>
2022-11-21 22:06:12 +0600

    added heroicon

--- src/Components/Subject/Subject.js
+++ src/Components/Subject/Subject.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {ArrowSmallRightIcon} from '@heroicons/react/24/solid'
 
 const Subject = ({sub}) => {
     return (
@@ -6,7 +7,8 @@ const Subject = ({sub}) => {
                 <img className='bg-zinc-400 w-72' src={sub.logo} alt="" />
             <div className='flex justify-between mt-4'>
                 <h2 className='p-2 text-2xl font-bold'>{sub.name}</h2>
-                <button className='bg-blue-600 text-white p-2 rounded-lg text-xl'>Start Practice</button>
+                <button className='bg-blue-600 text-white p-2 rounded-lg text-xl flex'>Start Practice <small><ArrowSmallRightIcon className='w-6 ml-2 mt-1'></ArrowSmallRightIcon></small> </button>
+               
             </div>
         </div>
     );
