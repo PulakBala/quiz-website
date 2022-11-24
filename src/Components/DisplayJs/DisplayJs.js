8d2f9b7fc6ef4c3c6cd0d9996a74fea729b0f4c0
PulakBala <pulakbala0524@gmail.com>
2022-11-24 11:05:47 +0600

    js correct answer show in toast

--- src/Components/DisplayJs/DisplayJs.js
+++ src/Components/DisplayJs/DisplayJs.js
@@ -1,19 +1,43 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 
 const DisplayJs = ({js}) => {
+    const {options,question,correctAnswer} = js;
+
+    const quizToast =(params) =>{
+        if(correctAnswer === params){
+          
+            toast('Correct Answer');
+        } else{
+           
+            toast('Wrong Answer');
+        }
+    }
     return (
         <div className='border w-9/12 my-8 bg-orange-100 py-10 rounded-lg'>
-            <h2 className='text-xl'> <small className='text-2xl font-bold-300'>Question:</small> {js.question}</h2>
+            <h2 className='text-xl'> <small className='text-2xl font-bold-300'>Question:</small> {question}</h2>
 
            <form className='grid grid-cols-2 place-content-center h-auto p-4 gap-10 mt-12'>
                
-                <p className='border border-red-900 h-20 p-4 flex items-center justify-center'> <input className=' h-12 w-4 mr-2' type="radio" id="html" name="fav_language"/> {js.options[0]}</p>
+                <p className='border border-red-900 h-20 p-4 flex items-center justify-center'>
+                <input onClick={()=> quizToast(options[0])} className=' h-12 w-4 mr-2' type="radio" id="html" name="fav_language"/> 
+                {options[0]}</p>
 
-                <p className='border border-red-900 p-4 flex items-center justify-center'> <input className=' h-12 w-4 mr-2' type="radio" id="html" name="fav_language"/> {js.options[1]}</p>
+                <p className='border border-red-900 p-4 flex items-center justify-center'>
+                <input onClick={()=> quizToast(options[1])} className=' h-12 w-4 mr-2' type="radio" id="html" name="fav_language"/> 
+                {options[1]}</p>
 
-                <p className='border border-red-900 p-4 h-20 flex items-center justify-center'> <input className=' h-12 w-4 mr-2' type="radio" id="html" name="fav_language"/> {js.options[2]}</p>
+                <p className='border border-red-900 p-4 h-20 flex items-center justify-center'> 
+                <input onClick={()=> quizToast(options[2])} className=' h-12 w-4 mr-2' type="radio" id="html" name="fav_language"/> 
+                {options[2]}</p>
 
-                <p className='border border-red-900 p-4 flex items-center justify-center'> <input className=' h-12 w-4 mr-2' type="radio" id="html" name="fav_language"/> {js.options[3]}</p>
+                <p className='border border-red-900 p-4 flex items-center justify-center'> 
+                <input onClick={()=> quizToast(options[3])} className=' h-12 w-4 mr-2' type="radio" id="html" name="fav_language"/> 
+                {options[3]}</p>
+
+                <ToastContainer
+                    autoClose={1000}
+                />
            </form>
         </div>
     );
