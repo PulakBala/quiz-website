86908e9825eae054873373ceb6c1b2f7c4694dae
PulakBala <pulakbala0524@gmail.com>
2022-11-24 11:26:43 +0600

    added git correct answer

--- src/Components/GitQuiz/GitQuiz.js
+++ src/Components/GitQuiz/GitQuiz.js
@@ -5,7 +5,6 @@ import DisplayGit from '../DisplayGit/DisplayGit';
 const GitQuiz = () => {
     const gitQiuz = useLoaderData();
     const displayGit = gitQiuz.data.questions;
-    console.log(gitQiuz.data.questions);
     return (
         <div className='grid justify-items-center'>
             <h2 className='text-3xl font-bold mt-8'>Quiz of Git</h2>
