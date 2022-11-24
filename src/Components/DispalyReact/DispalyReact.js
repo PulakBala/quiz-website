6eee64f02e06a48fb62c03c8a75a8d2a98801f80
PulakBala <pulakbala0524@gmail.com>
2022-11-24 12:18:06 +0600

    eye icon correct answer

--- src/Components/DispalyReact/DispalyReact.js
+++ src/Components/DispalyReact/DispalyReact.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
+import { EyeIcon } from '@heroicons/react/24/solid'
 import 'react-toastify/dist/ReactToastify.css';
 const DispalyReact = ({react}) => {
+    const {options,question,correctAnswer} = react;
+
+    const correctAns=() =>{ 
+        toast(correctAnswer)
+    }
 
     const quizToast = (param) => {
-     
-     if(react.correctAnswer === param) {
+    
+     if(correctAnswer === param) {
        
         toast('Correct Answer');
      } else{
@@ -14,29 +20,33 @@ const DispalyReact = ({react}) => {
      }
     }
     
+    
     return (
         <div className='border w-7/12 my-8 bg-orange-100 py-10 rounded-lg'>
-            <h2 className='text-xl'> <small className='text-2xl font-bold-300'>Question:</small> {react.question}</h2>
-
+           <div className='flex justify-evenly'>
+           <h2 className='text-xl'> <small className='text-2xl font-bold-300'>Question:</small> {question}  </h2>
+           <EyeIcon onClick={()=>correctAns()} className='h-10  pr-4 mt-14'/>
+           </div>
+           
            <form className='grid grid-cols-2 place-content-center h-auto p-4 gap-10 mt-12'>
                
                 <p  className='border border-red-900 h-20  flex items-center justify-center'> 
 
-                <input onClick={()=>quizToast(react.options[0])} className=' h-10 w-4 mr-2'  type="radio" id="html" name="fav_language"/> 
+                <input onClick={()=>quizToast(options[0])} className=' h-10 w-4 mr-2'  type="radio" id="html" name="fav_language"/> 
                
-                 {react.options[0]}</p>
+                 {options[0]}</p>
 
                 <p className='border border-red-900 flex items-center justify-center'> 
-                <input onClick={()=>quizToast(react.options[1])} className=' h-10 w-4 mr-2' type="radio" id="html" name="fav_language"/> 
-                {react.options[1]}</p>
+                <input onClick={()=>quizToast(options[1])} className=' h-10 w-4 mr-2' type="radio" id="html" name="fav_language"/> 
+                {options[1]}</p>
 
                 <p className='border border-red-900 h-20 flex items-center justify-center'>
-                <input  onClick={()=>quizToast(react.options[2])}className=' h-10 w-4 mr-2' type="radio" id="html" name="fav_language"/>
-                {react.options[2]}</p>
+                <input  onClick={()=>quizToast(options[2])}className=' h-10 w-4 mr-2' type="radio" id="html" name="fav_language"/>
+                {options[2]}</p>
 
                 <p className='border border-red-900 flex items-center justify-center'> 
-                <input  onClick={()=>quizToast(react.options[3])} className=' h-10 w-4 mr-2' type="radio" id="html" name="fav_language"/>
-                {react.options[3]}</p>
+                <input  onClick={()=>quizToast(options[3])} className=' h-10 w-4 mr-2' type="radio" id="html" name="fav_language"/>
+                {options[3]}</p>
 
                 <ToastContainer
                     autoClose={1000}
