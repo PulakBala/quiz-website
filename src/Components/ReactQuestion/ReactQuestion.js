8f65089699fb34c4918f05dd0c730ccc0caecb47
PulakBala <pulakbala0524@gmail.com>
2022-11-24 10:18:23 +0600

    added correct answer in react

--- src/Components/ReactQuestion/ReactQuestion.js
+++ src/Components/ReactQuestion/ReactQuestion.js
@@ -6,7 +6,7 @@ import DispalyReact from '../DispalyReact/DispalyReact';
 const ReactQuestion = () => {
     const reacts = useLoaderData();
     const reactDisplay = reacts.data.questions
-    console.log(reacts.data.questions)
+    
     return (
         <div className='grid justify-items-center'>
             <h2 className='text-3xl mt-8 font-bold'>Quiz of React</h2>
