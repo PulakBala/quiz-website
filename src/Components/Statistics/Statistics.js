a8211040fa3fe70c9a894641d8e54bb354ae647e
PulakBala <pulakbala0524@gmail.com>
2022-11-26 20:16:05 +0600

    added recharts

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { LineChart,  Line, YAxis, XAxis, Tooltip, Legend, CartesianGrid } from 'recharts';
 
 const Statistics = () => {
+    const totalQuestion = useLoaderData();
+    const data = totalQuestion.data;
     return (
         <div>
             <h2>This is Statistics page</h2>
+            <LineChart width={500} height={400} data={data}>
+            <Line type="monotone" dataKey="name" />
+            <Line type="monotone" datakey="total"/>
+            <CartesianGrid strokeDasharray="3 3"/>
+            <XAxis dataKey="name"/>
+            <YAxis dataKey="total"/>
+            <Tooltip/>
+            <Legend/>
+         </LineChart>
         </div>
     );
 };
